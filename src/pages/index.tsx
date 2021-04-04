589413a616b43d1ddde9e60cd8c275bfd7b27b57
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-04-04 15:35:03 -0300

    boilerplate finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-export default function Home    ({ title = 'React avançado' }) {
+export default function Home({ title = 'React avançado' }) {
   return (
     <div className={styles.container}>
       <Head>
