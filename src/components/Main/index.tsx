1203f764f92679106f0d97d50d92990d4fb1c779
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-04-04 19:34:51 -0300

    ajustando boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,7 @@
 const Main = () => (
   <main>
     <h1>React avançado</h1>
+    <p>Michel Ribeiro...</p>
   </main>
 )
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
-import '../../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
