d1e07af48eb2fa8c0a46ebca6996a0f8ae70fc6d
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-04-04 18:06:15 -0300

    teste

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,7 +6,7 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançadasdasdo/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
