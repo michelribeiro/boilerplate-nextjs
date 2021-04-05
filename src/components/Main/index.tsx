1f4ef507702ad3bead223c4e83989920b82e1551
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-04-04 22:33:15 -0300

    adicionando storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-    <p>Michel Ribeiro...</p>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="React avançado" />
+    <S.Title>React avançado</S.Title>
+    <S.Description>Michel Ribeiro...</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Seja um bom desenvolvedor"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,12 +3,18 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
-    // expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
